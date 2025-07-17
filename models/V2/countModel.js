a34c663f3381e7a03465cdbcbb566db018dca481
mariodenny <dennym.channel@gmail.com>
2025-07-17 11:34:54 +0700

    fix model,fix route by machine id

--- models/V2/countModel.js
+++ models/V2/countModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const countSchema = new mongoose.Schema({
-  machineId: { type: mongoose.Schema.Types.ObjectId, ref: "Machine", required: true },
+  machineId: { type: mongoose.Schema.Types.ObjectId, ref: "Machine" },
   disetujui: { type: Number, default: 0 },
   ditolak: { type: Number, default: 0 },
   menunggu: { type: Number, default: 0 },
   waktu: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Count", countSchema);
+module.exports = mongoose.model("CountV2", countSchema);
--- routes/V2/routeCount.js
+++ routes/V2/routeCount.js
@@ -4,7 +4,7 @@ const countController = require("../../controllers/V2/countController");
 const verifyToken = require("../../middleware/verifyToken");
 
 router.get("/counts", verifyToken, countController.getAllCounts);
-router.get("/reports", verifyToken, countController.getUsageReport)
-router.get("/counts/machine/:machineId", verifyToken, countController.getCountByMachine);
+router.get("/reports/:machineId", verifyToken, countController.getUsageReport)
+router.get("/counts/:machineId", verifyToken, countController.getCountByMachine);
 
 module.exports = router;
