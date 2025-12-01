2c84fb6a26aa1954fac5ddecb38fea2b80e0a095
mariodeny <dennym.channel@gmail.com>
2025-12-01 20:13:10 +0700

    REMOVE token

--- routes/routeMachine.js
+++ routes/routeMachine.js
@@ -8,7 +8,7 @@ const upload = require("../middleware/upload")
 
 // CRUD Machine
 router.post("/machines", upload.single("image"), verifyToken, machineController.createMachine);
-router.get("/machines", verifyToken, machineController.getMachines);
+router.get("/machines", machineController.getMachines);
 router.put("/machines/:id", upload.single("image"), verifyToken, machineController.updateMachine);
 router.delete("/machines/:id", verifyToken, machineController.deleteMachine);
 router.get("/machine/:id",verifyToken,machineController.getMachineById)
